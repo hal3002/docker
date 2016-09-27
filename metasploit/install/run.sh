397973aedde5a199ff1fd83f660f5c4d3c79a542
hal3002 <hal@nutsec.org>
2016-09-27 11:23:20 -0400

    Adding aliases

--- metasploit/install/run.sh
+++ metasploit/install/run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 msfdb init &> /dev/null
-sleep 1
-
-#if [ -e /root/.msf4 ]; then
-#   if [ ! -L /root/.msf4 ]; then
-#      echo "/root/.msf4 already exists and is not a symlink"
-#   fi
-#else
-#   ln -s /home/msf/.msf4 /root/.msf4
-#fi
-
+echo "Waiting to let the database start"
+sleep 5
 sudo /usr/bin/msfconsole
